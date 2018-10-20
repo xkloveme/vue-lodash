121122fdd6076cc93b60d7fc696488854e2002a3
superDragon <admin@jixiaokang.com>
2018-10-20 11:43:35 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    {{list}}
+    <p>{{res}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      list: ['a', 'b', 'c', 'd'],
+      res: null
+    }
+  },
+  mounted () {
+    this.res = this._.chunk(this.list, 3)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
